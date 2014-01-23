7a0caed532905600e126ce3767d818e7b5d7601a
Zack Loveless <bhmath@gmail.com>
2014-01-23 14:57:10 -0700

    Add more vector operations :)

--- Tests/VectorLib.Tests/Vector3Fixture.cs
+++ Tests/VectorLib.Tests/Vector3Fixture.cs
@@ -24,15 +24,51 @@
 
 			Assert.That(SUT.Length, Is.EqualTo(value));
 		}
-
 		[Test]
-		public void Vector3MultiplicationOperator_ImplementsTheDotOperator_ShouldBeImplementedProperly()
+		public void Operator_VectorTimesVector_ShouldReturnTheDotProductOfBothVectors()
 		{
 			var vector = new Vector3(3, 7, -2);
 
 			var value  = SUT * vector;
 
 			Assert.That(value, Is.EqualTo(30));
+		}
+
+		[Test]
+		public void Operator_VectorTimesScalar_ShouldReturnVector3MultipliedByScalar()
+		{
+			var expected = new Vector3(10, 20, 10);
+			var value    = SUT * 5;
+
+			Assert.That(value, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void ToString_ShouldReturnVectorNotation()
+		{
+			var expected = "<2,4,2>";
+			var value    = SUT.ToString();
+
+			Assert.That(value, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Equals_ShouldEqualEquivalentVector3()
+		{
+			var value = new Vector3(2, 4, 2);
+
+			Assert.That(SUT, Is.AssignableTo(typeof (IEquatable<Vector3>)));
+			Assert.That(SUT, Is.EqualTo(value));
+		}
+
+		[Test]
+		public void Vector3Cross_ShouldBeImplementedProperly()
+		{
+			var expected = new Vector3(-22, 10, 2);
+			var vector   = new Vector3(3, 7, -2);
+			var value    = Vector3.Cross(SUT, vector);
+
+			Assert.That(value, Is.EqualTo(expected));
 		}
 	}
 
--- VectorLib/Vector3.cs
+++ VectorLib/Vector3.cs
@@ -8,9 +8,9 @@ namespace VectorLib
 {
 	using System;
 
-	public class Vector3
+	public struct Vector3 : IEquatable<Vector3>
 	{
-		public Vector3(double x, double y, double z)
+		public Vector3(double x, double y, double z) : this()
 		{
 			X = x;
 			Y = y;
@@ -28,14 +28,20 @@ namespace VectorLib
 			get { return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2)); }
 		}
 
-		#region
-
-		public static double Cross(Vector3 a, Vector3 b)
+		#region Static modifiers
+		
+		public static Vector3 Cross(Vector3 a, Vector3 b)
 		{
-			throw new NotImplementedException();
+			double x, y, z;
+
+			x = a.Y * b.Z - a.Z * b.Y;
+			y = (a.X * b.Z - a.Z * b.X) * -1;
+			z = a.X * b.Y - a.Y * b.X;
+
+			return new Vector3(x, y, z);
 		}
 
-		public static double Cross(Vector3 a, Vector3 b, Vector3 c)
+		public static double Distance(Vector3 a, Vector3 b)
 		{
 			throw new NotImplementedException();
 		}
@@ -49,11 +55,53 @@ namespace VectorLib
 			return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
 		}
 
+		/// <summary>
+		/// Implements the dot product for two <see cref="T:Vector3" /> instances.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
 		public static double operator *(Vector3 a, Vector3 b)
 		{
 			// Dot product
+			return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+		}
 
-			return (a.X * b.X) + (a.Y * b.Y) + (a.Z * b.Z);
+		public static Vector3 operator *(Vector3 a, double s)
+		{
+			// Vector-Scalar multiplcation
+			return new Vector3(a.X * s, a.Y * s, a.Z * s);
+		}
+
+		#endregion
+
+		#region Implementation of IEquatable<Vector3>
+
+		/// <summary>
+		/// Indicates whether the current object is equal to another object of the same type.
+		/// </summary>
+		/// <returns>
+		/// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+		/// </returns>
+		/// <param name="other">An object to compare with this object.</param>
+		public bool Equals(Vector3 other)
+		{
+			return X == other.X && Y == other.Y && Z == other.Z;
+		}
+
+		#endregion
+
+		#region Overrides of ValueType
+
+		/// <summary>
+		/// Returns the fully qualified type name of this instance.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.String"/> containing a fully qualified type name.
+		/// </returns>
+		public override string ToString()
+		{
+			return string.Format("<{0}>", string.Join(",", X, Y, Z));
 		}
 
 		#endregion
